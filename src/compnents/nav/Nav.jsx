2c331a84ff38f9e3665997999ff3a4e7ab46f70c
Nassahel Elias <79935315+nassahel@users.noreply.github.com>
2023-08-04 21:52:58 -0300

    se creo componente de busqueda y se lo agrego al nav

--- src/compnents/nav/Nav.jsx
+++ src/compnents/nav/Nav.jsx
@@ -1,10 +1,14 @@
 import './nav.css'
 import logo from '../assets/logo.png'
+import Search from '../buscador/search'
+
+
 
 export function Nav()  {
     return ( 
         <div className="nav">
             <img src={logo}alt="" />
+            <Search></Search>
             <ul>
                 <li><a href="">Hombre</a></li>
                 <li><a href="">Mujeres</a></li>
